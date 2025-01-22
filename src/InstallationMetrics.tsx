2449be4264709c9d661e9269255e45bf2e048c69
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2025-01-22 16:27:42 -0500

    Add data fetching logic to InstallationMetrics component

--- src/InstallationMetrics.tsx
+++ src/InstallationMetrics.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+
 function InstalltionMetrics(){
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch("https://hub.dataverse.org/api/installations/status");
+        }
+    }, []);
+
     return (
         <div>
             <h1>Installation Metrics</h1>
